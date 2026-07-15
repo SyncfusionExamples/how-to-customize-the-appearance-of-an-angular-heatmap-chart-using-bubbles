d6ed2113b9717eb6fe54a18ed581409ba1ce4984
Kokila Poovendran <kokila.poovendran@syncfusion.com>
2026-07-15 13:06:47 +0530

    1040273: Addressed the dependabot issue

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,10 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
 import { HeatMapModule } from '@syncfusion/ej2-angular-heatmap';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, HeatMapModule],
+  imports: [HeatMapModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,8 +1,14 @@
-import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
+import { ApplicationConfig, APP_ID, provideZoneChangeDetection } from '@angular/core';
 import { provideRouter } from '@angular/router';
+import { provideClientHydration } from '@angular/platform-browser';
 
 import { routes } from './app.routes';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes)]
+  providers: [
+    provideZoneChangeDetection({ eventCoalescing: true }),
+    provideRouter(routes),
+    { provide: APP_ID, useValue: 'myapp-unique-secure-id' },
+    provideClientHydration()
+  ]
 };
